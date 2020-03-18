61882c4b3b5250fa98fed9855434309c039249ca
salnuraqidah <salnuraqidah@gmail.com>
2020-03-18 12:01:30 +0700

    update

---
+++ Kumpulan Tugas/tugas php/tugas8_webdasar_salamah/infoGempa.php
@@ -0,0 +1,19 @@
+<?php
+//sertakan file classnya
+require_once 'gempa.php';
+
+$g1 = new Gempa('Yogyakarta',3);
+$g2 = new Gempa('Tokyo',6.8);
+$g3 = new Gempa('Sukabumi',2.5);
+$g4 = new Gempa('Jawa Tengah',1.5);
+$g5 = new Gempa('Hawai',8.2);
+$g6 = new Gempa('China',4.4);
+$g7 = new Gempa('Thailand',9.3);
+$g8 = new Gempa('Laos',3.4);
+$g9 = new Gempa('Kamboja',6.3);
+$g10 = new Gempa('Lampung',4.3);
+ 
+
+$g1->cetak(); $g2->cetak(); $g3->cetak(); $g4->cetak();
+$g5->cetak(); $g6->cetak(); $g7->cetak(); $g8->cetak();
+$g9->cetak(); $g10->cetak();
